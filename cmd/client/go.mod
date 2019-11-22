966e8c36becf832728bc1628ed92242570464b16
Mark Chmarny <mark@chmarny.com>
2019-11-22 01:01:41 +0000

    init

---
+++ cmd/client/go.mod
@@ -0,0 +1,9 @@
+module github.com/mchmarny/distributed-echo/cmd/client
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	golang.org/x/net v0.0.0-20191119073136-fc4aabc6c914
+	google.golang.org/grpc v1.25.1
+)
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,20 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// [START run_pubsub_server]
-
-// Sample grpc-service is a Cloud Run service client which sends messages over gRPC.
 package main
 
 import (
@@ -26,7 +9,7 @@ import (
 	"time"
 
 	ptypes "github.com/golang/protobuf/ptypes"
-	pb "github.com/mchmarny/grpc-sample/pkg/api/v1"
+	pb "github.com/mchmarny/distributed-echo/pkg/api/v1"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -61,7 +44,7 @@ func main() {
 		logger.Fatalf("Failed to dial: %v", err)
 	}
 	defer conn.Close()
-	client := pb.NewMessageServiceClient(conn)
+	client := pb.NewPingServiceClient(conn)
 
 	if *streamSize == 0 {
 		send(client)
@@ -70,54 +53,14 @@ func main() {
 	}
 }
 
-func send(client pb.MessageServiceClient) {
+func send(client pb.PingServiceClient) {
 	ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
 	defer cancel()
 	resp, err := client.Send(ctx, &pb.Request{
-		Message: *message,
+
 	})
 	if err != nil {
 		logger.Fatalf("Error while executing Send: %v", err)
 	}
 	logger.Printf("Unary Request/Unary Response\n Sent:\n  %s\n Response:\n  %+v", *message, resp)
-}
-
-func sendStream(client pb.MessageServiceClient) {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
-	defer cancel()
-
-	stream, err := client.SendStream(ctx)
-	if err != nil {
-		logger.Fatalf("Error client (%v) PingStream: %v", client, err)
-	}
-
-	waitCh := make(chan struct{})
-	go func() {
-		logger.Println("Unary Request/Stream Response")
-		for {
-			in, err := stream.Recv()
-			if err == io.EOF {
-				close(waitCh)
-				return
-			}
-			if err != nil {
-				logger.Fatalf("Failed to receive a response: %v", err)
-			}
-			c := in.GetContent()
-			logger.Printf("  Stream[%d] - Server time: %s",
-				c.GetIndex(), ptypes.TimestampString(c.GetReceivedOn()))
-		}
-	}()
-
-	i := 0
-	for i < *streamSize {
-		if err := stream.Send(&pb.Request{
-			Message: *message,
-		}); err != nil {
-			logger.Fatalf("Failed to Send: %v", err)
-		}
-		i++
-	}
-	stream.CloseSend()
-	<-waitCh
 }
