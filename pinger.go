9adafea5838bc736c075c7085a3eded44f72441a
Mark Chmarny <mark@chmarny.com>
2019-11-29 12:06:13 -0800

    non fatal metric error

--- pinger.go
+++ pinger.go
@@ -66,7 +66,8 @@ func pingNode(ctx context.Context, target *EchoNode) (dur int64, err error) {
 		"target": target.Region,
 	}
 	if err := metric.MakeClient(ctx).Publish(ctx, "echo-duration", echoDuration, labels); err != nil {
-		return echoDuration, fmt.Errorf("Error while publishing metrics: %v", err)
+		// more then 1 metric per sec will cause an error
+		logger.Printf("Non-fatal error while publishing metrics: %v", err)
 	}
 
 	return echoDuration, nil
